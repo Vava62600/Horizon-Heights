bfde9f061fbe6b973bf3d5e19cf11d501828151b
Vava_62600 <120785753+Vava62600@users.noreply.github.com>
2023-09-06 14:40:53 +0000

    V0.1

--- Website/interaction.js
+++ Website/interaction.js
@@ -1,37 +1,11 @@
-  // Sélectionnez les boutons par leur ID ou une autre méthode appropriée
-  const blogButton = document.getElementById('blog');
-  const downloadButton = document.getElementById('download');
-  const aboutButton = document.getElementById('about');
+
   const popupButton = document.getElementById('popup');
 
-  // Définissez les fonctions à exécuter lors du clic sur les boutons
-  function ouvrirBlog() {
-    const url1 = 'blog.html';
-    window.open(url1);
-    ouvrirPopup();
-  }
-
-  function ouvrirDownload() {
-    const url2 = 'download.html';
-    window.open(url2);
-    ouvrirPopup();
-  }
-
-  function ouvrirAbout() {
-    const url3 = 'about.html';
-    window.open(url3);
-    ouvrirPopup();
-  }
-
-  // Ajoutez des écouteurs d'événements aux boutons pour détecter les clics
-  blogButton.addEventListener('click', ouvrirBlog);
-  downloadButton.addEventListener('click', ouvrirDownload);
-  aboutButton.addEventListener('click', ouvrirAbout);
   popupButton.addEventListener('click', ouvrirPopup);
 
   function ouvrirPopup() {
-    const url = 'login.html';
-    const name = 'SRP Play | Login';
+    const url = 'Login.html';
+    const name = 'Horizon Heights | Login';
     // Définissez les options pour le popup
     const width = 508;
     const height = 590;
